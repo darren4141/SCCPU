0b867fc3dcb2fa58594ea819675bdbd6cd0c50f8
Darren Liu <darrrenliu@gmail.com>
2026-03-05 15:47:02 -0500

    hello world

---
+++ tb_adder.v
@@ -0,0 +1,24 @@
+`timescale 1ns/1ps
+
+module tb_adder;
+
+reg [31:0] a;
+reg [31:0] b;
+wire [31:0] sum;
+
+adder uut(a, b, sum);
+
+initial begin
+    $dumpfile("wave.vcd");
+    $dumpvars(0, tb_adder);
+
+    a = 5; b = 3;
+    #10;
+
+    a = 10; b = 7;
+    #10;
+
+    $finish;
+end
+
+endmodule
