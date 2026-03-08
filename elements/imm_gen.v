a95209efa570984537c11b6a7f1a6e514235b9c3
Darren Liu <darrrenliu@gmail.com>
2026-03-08 00:23:17 -0500

    improved imm gen block

--- elements/imm_gen.v
+++ elements/imm_gen.v
@@ -8,34 +8,24 @@ module imm_gen (
 
   wire [6:0] opcode = inst[6:0];
   wire sign = inst[31];
-  reg [6:0] format;
 
   always @(*) begin
-    format = `FORMAT_INVALID;
     imm = 32'b0;
 
     case (opcode)
-      `OPCODE_ARITH_OP: format = `FORMAT_R;
-      `OPCODE_ARITH_OP_IMM: format = `FORMAT_I;
-      `OPCODE_LOAD: format = `FORMAT_I;
-      `OPCODE_JALR: format = `FORMAT_I;
-      `OPCODE_ENV: format = `FORMAT_I;
-      `OPCODE_STORE: format = `FORMAT_S;
-      `OPCODE_BRANCH: format = `FORMAT_B;
-      `OPCODE_LUI: format = `FORMAT_U;
-      `OPCODE_AUIPC: format = `FORMAT_U;
-      `OPCODE_JAL: format = `FORMAT_J;
-      default: format = `FORMAT_INVALID;
-    endcase
-
-    case (format)
-      `FORMAT_R: imm = 32'b0;
-      `FORMAT_I: imm = {{20{sign}}, inst[31:20]};
-      `FORMAT_S: imm = {{20{sign}}, inst[31:25], inst[11:7]};
-      `FORMAT_B: imm = {{19{sign}}, inst[31], inst[7], inst[30:25], inst[11:8], 1'b0};
-      `FORMAT_U: imm = {inst[31:12], {12{1'b0}}};
-      `FORMAT_J: imm = {{11{sign}}, inst[31], inst[19:12], inst[20], inst[30:21], 1'b0};
-      `FORMAT_INVALID: imm = 32'b0;
+      // R-format
+      `OPCODE_ARITH_OP: imm = 32'b0;
+      // I-format
+      `OPCODE_ARITH_OP_IMM, `OPCODE_LOAD, `OPCODE_JALR, `OPCODE_ENV:
+      imm = {{20{sign}}, inst[31:20]};
+      // S-format
+      `OPCODE_STORE: imm = {{20{sign}}, inst[31:25], inst[11:7]};
+      // B-format
+      `OPCODE_BRANCH: imm = {{19{sign}}, inst[31], inst[7], inst[30:25], inst[11:8], 1'b0};
+      // U-format
+      `OPCODE_LUI, `OPCODE_AUIPC: imm = {inst[31:12], {12{1'b0}}};
+      // J-format
+      `OPCODE_JAL: imm = {{11{sign}}, inst[31], inst[19:12], inst[20], inst[30:21], 1'b0};
       default: imm = 32'b0;
     endcase
 
